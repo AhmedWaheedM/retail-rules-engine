ea2689b2039305d0a5c3dea27e8b98df650b7b17
AhmedWaheedM <ahmedwaheed2003@gmail.com>
2026-04-22 23:30:28 +0200

    feat(rules): add app channel and visa payment discounts, keep existing rule set

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,31 @@
-import infrastructure.DataLoader
-import infrastructure.DataWriter
-import infrastructure.Logger
-import rules.QualifyingRules
-import rules.Engine
-import scala.util.Success
-import scala.util.Failure
+import infrastructure.{DataLoader, DataWriter, Logger}
+import rules.{QualifyingRules, Engine}
+import scala.util.{Success, Failure}
+
 object Main extends App {
-  val csvFilePath = "data\\TRX1000.csv"
-  val pipeline = for {
-    _ <- Logger.log(Logger.INFO, "Starting the transaction processing pipeline.")
-    transactions <- DataLoader.loadTransactions(csvFilePath)
-    _ <- Logger.log(Logger.INFO, s"Loaded ${transactions.size} transactions from $csvFilePath.")
-    processed = Engine.processTransactions(transactions, QualifyingRules.allRules)
-    _ <- DataWriter.writeResults(processed)
-    _ <- Logger.log(Logger.INFO, s"Successfully processed and stored ${processed.size} transactions")
-  } yield processed
-  pipeline match {
-    case Success(data) => Logger.log(Logger.INFO, s"PIPELINE COMPLETED: Processed ${data.size} transactions successfully.")
-      println(s"SUCCESS! Processed ${data.size} transactions. Check rules_engine.log")
-    case Failure(error) => Logger.log(Logger.ERROR, s"PIPELINE FAILED: ${error.getMessage}")
-      println(s"CRITICAL ERROR: ${error.getMessage}")
+  
+  val csvFilePath = "data\\TRX10M.csv" 
+  
+  Logger.log(Logger.INFO, "Starting the SCALED transaction processing pipeline.")
+
+  val pipelineResult = DataLoader.loadTransactions(csvFilePath) { transactionStream =>
+    
+    val processedStream = Engine.processTransactions(transactionStream, QualifyingRules.allRules)
+    
+    DataWriter.writeResults(processedStream)
   }
+
+ pipelineResult match {
+  case Success(Success(totalSaved)) =>
+    Logger.log(Logger.INFO, s"PIPELINE COMPLETED: Processed and saved $totalSaved transactions.")
+    println(s"SUCCESS! Saved $totalSaved transactions. Check rules_engine.log")
+
+  case Success(Failure(dbError)) =>
+    Logger.log(Logger.ERROR, s"DATABASE FAILED: ${dbError.getMessage}")
+    println(s"CRITICAL ERROR: Database failed - ${dbError.getMessage}")
+
+  case Failure(fileError) =>
+    Logger.log(Logger.ERROR, s"FILE LOAD FAILED: ${fileError.getMessage}")
+    println(s"CRITICAL ERROR: File failed - ${fileError.getMessage}")
+}
 }
--- src/main/scala/rules/QualifyingRules.scala
+++ src/main/scala/rules/QualifyingRules.scala
@@ -2,28 +2,41 @@ package rules
 import models.Transaction
 import java.time.Month
 object QualifyingRules {
+    // Factory methods to create rules based on different criteria
+
     def createCategoryRule(targetCategory: String, discountRate: Double): Transaction => Double = {
         t => if (t.productName.toLowerCase.contains(targetCategory.toLowerCase)) discountRate else 0.0
-}
+    } // to create a category-based rule
+
     def createDateRule(targetMonth: Month, targetDay: Int, discountRate: Double): Transaction => Double = {
         t => {
             val date = t.timestamp.toLocalDate
             if (date.getMonth == targetMonth && date.getDayOfMonth == targetDay) discountRate else 0.0
         }
-    }
+    } // to create a date-based rule
 
     def createTieredQuantityRule(tiers: List[(Int, Double)]): Transaction => Double = {
         val sortedTiers = tiers.sortBy  {case (threshold, _) => -threshold}
         t => sortedTiers.find { case (threshold, _) => t.quantity >= threshold }.map(_._2).getOrElse(0.0) }
-    
-    // Defining rules 
+
+    def createPaymentRule(targetPaymentMethod: String, discountRate: Double): Transaction => Double = {
+        t => if (t.paymentMethod.toLowerCase == targetPaymentMethod.toLowerCase) discountRate else 0.0
+    } // to create a payment method-based rule
+
+    // Specific rules requiring custom logic
     val expirationRule: Transaction => Double = t => {
         val daysLeft = t.daysToExpiry
         if (daysLeft >= 0 && daysLeft < 30) (30 - daysLeft) / 100.0 else 0.0    
-        }
+        } // to create a rule based on days to expiry
+    
+    val appRule: Transaction => Double = t => if (t.channel.toLowerCase == "app") Math.ceil(t.quantity / 5.0) * 0.05 else 0.0 // to create a rule based on sales channel
+
+// List of all rules to be applied in the engine
 val allRules: List[Transaction => Double] = List(
     expirationRule,
+    appRule,
     createCategoryRule("cheese", 0.10),
+    createPaymentRule("visa", 0.05),
     createCategoryRule("wine", 0.05),
     createDateRule(Month.MARCH, 23, 0.50),
     createTieredQuantityRule(List( (15, 0.10), (10, 0.07), (6, 0.05) ))
